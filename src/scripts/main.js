906a3be4bf7d4e766957060ef48ac97bd26e2370
datamadic <xavier@openfin.co>
2014-10-28 18:08:15 -0400

    check for debrowserify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,6 @@ var underbar = require('./underbar.js'),
 		observe = require('./observe.js'),
 		scrollBar = require('./scroll-bar.js'),
 		rangeAdapter = require('./range-adapter.js');
+
+module.exports.RangeAdapter = rangeAdapter;
 		
